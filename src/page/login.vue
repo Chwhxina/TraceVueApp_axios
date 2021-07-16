083dbfa6960a7995fe006eee217ba6750df36a88
王浩翔 <wanghaoxiang@wanghaoxiangdeMacBook-Air.local>
2021-07-16 15:33:36 +0800

    Initial commit

---
+++ src/page/login.vue
@@ -0,0 +1,129 @@
+<template>
+  	<div class="login_page fillcontain">
+	  	<transition name="form-fade" mode="in-out">
+	  		<section class="form_contianer" v-show="showLogin">
+		  		<div class="manage_tip">
+		  			<p>武大蚂蚁链食品朔源系统</p>
+		  		</div>
+		    	<el-form :model="loginForm" :rules="rules" ref="loginForm">
+					<el-form-item prop="username">
+						<el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
+					</el-form-item>
+					<el-form-item prop="password">
+						<el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
+					</el-form-item>
+					<el-form-item>
+				    	<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
+				  	</el-form-item>
+				</el-form>
+				<p class="tip">温馨提示：</p>
+				<p class="tip">本系统仅为武大蚂蚁链教学专用，请勿用于其他场景。</p>
+	  		</section>
+	  	</transition>
+  	</div>
+</template>
+
+<script>
+	import {login} from '@/api/getData'
+	import {mapActions, mapState} from 'vuex'
+	import {mapMutations} from 'vuex';
+
+	export default {
+	    data(){
+			return {
+				loginForm: {
+					username: '',
+					password: '',
+				},
+				rules: {
+					username: [
+			            { required: true, message: '请输入用户名', trigger: 'blur' },
+			        ],
+					password: [
+						{ required: true, message: '请输入密码', trigger: 'blur' }
+					],
+				},
+				showLogin: false,
+			}
+		},
+		mounted(){
+			this.showLogin = true;
+		},
+		computed: {
+		},
+		methods: {
+			...mapMutations(['changeLogin']),
+			async submitForm(formName) {
+				let _this = this;
+				this.$refs[formName].validate(async (valid) => {
+					if (valid) {
+						login({name: this.loginForm.username, password: this.loginForm.password})
+						.then(res => {
+							if (res.rspCode == '000000') {
+									this.$message({
+		                        	type: 'success',
+		                        	message: '登录成功'
+		                    	});
+		                    	console.log(res.rspData.token);
+		                    	_this.changeLogin({ Authorization: res.rspData.token});
+								this.$router.push('home');
+							} else {
+								this.$message({
+		                        	type: 'error',
+		                        	message: res.rspMsg
+		                    	});
+							}
+						});
+					} else {
+						this.$notify.error({
+							title: '错误',
+							message: '请输入正确的用户名密码',
+							offset: 100
+						});
+						return false;
+					}
+				});
+			},
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	@import '../style/mixin';
+	.login_page{
+		background-color: #324057;
+	}
+	.manage_tip{
+		position: absolute;
+		width: 100%;
+		top: -100px;
+		left: 0;
+		p{
+			font-size: 34px;
+			color: #fff;
+		}
+	}
+	.form_contianer{
+		.wh(350px, 210px);
+		.ctp(350px, 210px);
+		padding: 25px;
+		border-radius: 5px;
+		text-align: center;
+		background-color: #fff;
+		.submit_btn{
+			width: 100%;
+			font-size: 16px;
+		}
+	}
+	.tip{
+		font-size: 12px;
+		color: red;
+	}
+	.form-fade-enter-active, .form-fade-leave-active {
+	  	transition: all 1s;
+	}
+	.form-fade-enter, .form-fade-leave-active {
+	  	transform: translate3d(0, -50px, 0);
+	  	opacity: 0;
+	}
+</style>
